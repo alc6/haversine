9381b70f9e0f639a2f736185473aac0e243b96e5
AlexClemente <clemente.alexis@gmail.com>
2020-08-17 20:26:56 +0200

    IntermediatePointTo function

--- haversine.go
+++ haversine.go
@@ -20,6 +20,11 @@ func degreesToRadians(d float64) float64 {
 	return d * math.Pi / 180
 }
 
+// radiansToDegrees converts from radians to degrees.
+func radiansToDegrees(d float64) float64 {
+	return d *  180 / math.Pi
+}
+
 // Distance calculates the shortest path between two coordinates on the surface
 // of the Earth. This function returns two units of measure, the first is the
 // distance in miles, the second is the distance in kilometers.
@@ -41,4 +46,33 @@ func Distance(p, q Coord) (mi, km float64) {
 	km = c * earthRaidusKm
 
 	return mi, km
+}
+
+// Returns the point at given fraction between ‘this’ point and given point.
+func (inpoint Coord) IntermediatePointTo(point Coord, fraction float64) (newPt Coord) {
+	// https://www.movable-type.co.uk/scripts/latlong.html
+	lat1 := degreesToRadians(inpoint.Lat)
+	lon1 := degreesToRadians(inpoint.Lon)
+	lat2 := degreesToRadians(point.Lat)
+	lon2 := degreesToRadians(point.Lon)
+
+	// distance between points
+	deltaLat := lat2 - lat1
+	deltaLon := lon2 - lon1
+	a := math.Sin(deltaLat/2)*math.Sin(deltaLat/2) + math.Cos(lat1)*math.Cos(lat2)*math.Sin(deltaLon/2)*math.Sin(deltaLon/2)
+	dBetween := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	A := math.Sin((1-fraction)*dBetween) / math.Sin(dBetween)
+	B := math.Sin(fraction*dBetween) / math.Sin(dBetween)
+
+	x := A*math.Cos(lat1)*math.Cos(lon1) + B*math.Cos(lat2)*math.Cos(lon2)
+	y := A*math.Cos(lat1)*math.Sin(lon1) + B*math.Cos(lat2)*math.Sin(lon2)
+	z := A*math.Sin(lat1) + B*math.Sin(lat2)
+
+	lat3Rad := math.Atan2(z, math.Sqrt(x*x+y*y))
+	lon3Rad := math.Atan2(y, x)
+
+	newPt.Lat = radiansToDegrees(lat3Rad)
+	newPt.Lon = radiansToDegrees(lon3Rad)
+	return newPt
 }
